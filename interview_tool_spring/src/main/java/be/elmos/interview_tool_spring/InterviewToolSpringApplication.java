60a3a29e5a90181a41df3e40c52664ae69d494b0
Yannick Delandmeter  | Consultant <yannick.delandmeter@elmos.be>
2023-09-11 15:34:53 +0200

    Spring project initialization

---
+++ interview_tool_spring/src/main/java/be/elmos/interview_tool_spring/InterviewToolSpringApplication.java
@@ -0,0 +1,13 @@
+package be.elmos.interview_tool_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewToolSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewToolSpringApplication.class, args);
+	}
+
+}
---
+++ interview_tool_spring/src/test/java/be/elmos/interview_tool_spring/InterviewToolSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package be.elmos.interview_tool_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewToolSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
